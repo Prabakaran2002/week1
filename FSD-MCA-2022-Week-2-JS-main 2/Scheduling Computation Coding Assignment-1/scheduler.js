657c7a645b983643e77d0497a58aa9de604cb060
Prabakaran2002 <111951107+Prabakaran2002@users.noreply.github.com>
2022-12-16 21:57:11 +0530

    Add files via upload

---
+++ FSD-MCA-2022-Week-2-JS-main 2/Scheduling Computation Coding Assignment-1/scheduler.js
@@ -0,0 +1,9 @@
+//Write a function here to display "Hello World" on the console
+function sayhello(){
+    console.log("hello world!");
+
+}
+setTimeout(sayhello,3000);
+
+//Write another function here that will delay the execution of the 1st function by 3 seconds
+
